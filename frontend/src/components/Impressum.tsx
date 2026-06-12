0fcf9399972cfb209ea6db09608c79e8b8c7fb23
DanielJMoser <daniel.moser@mci.edu>
2026-06-12 11:14:34 +0200

    feat(impressum) add impressum

--- frontend/src/components/Impressum.tsx
+++ frontend/src/components/Impressum.tsx
@@ -14,9 +14,9 @@ const Impressum: React.FC = () => {
       </h2>
       <div className={`impressum__content${open ? ' impressum__content--open' : ''}`} inert={!open}>
         <div className="impressum__inner">
-          <p><b>Medieninhaber &amp; Herausgeber:</b> [VOR- UND NACHNAME EINTRAGEN]</p>
-          <p><b>Wohnort:</b> Innsbruck, Österreich</p>
-          <p><b>Kontakt:</b> [E-MAIL-ADRESSE EINTRAGEN]</p>
+          <p><b>Medieninhaber &amp; Herausgeber:</b> Daniel Moser</p>
+          <p><b>Wohnort:</b> Brunecker Strasse 2a, 6020 Innsbruck, Tirol, Österreich</p>
+          <p><b>Kontakt:</b> wosweat@protonmail.com</p>
           <p>
             Offenlegung gem. §25 MedienG: wosweat ist ein privates, nicht-kommerzielles
             Informationsangebot über Veranstaltungen in Innsbruck. Veranstaltungsdaten
